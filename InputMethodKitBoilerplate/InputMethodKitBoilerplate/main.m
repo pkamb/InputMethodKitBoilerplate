bc9084502bfb4056178e5459cc7a2204997e3858
Peter Kamb <peterkamb@gmail.com>
2016-06-21 20:42:40 -0700

    Add IMKServer initialization to `main.m`

--- InputMethodKitBoilerplate/InputMethodKitBoilerplate/main.m
+++ InputMethodKitBoilerplate/InputMethodKitBoilerplate/main.m
@@ -7,7 +7,21 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <InputMethodKit/InputMethodKit.h>
+
+const NSString *kConnectionName = @"InputMethodKit_Boilerplate_Connection";
+
+IMKServer *server;
+IMKCandidates *candidatesWindow;
 
 int main(int argc, const char * argv[]) {
-    return NSApplicationMain(argc, argv);
+    @autoreleasepool {
+        server = [[IMKServer alloc] initWithName:(NSString *)kConnectionName bundleIdentifier:[NSBundle mainBundle].bundleIdentifier];
+        
+        candidatesWindow = [[IMKCandidates alloc] initWithServer:server panelType:kIMKSingleColumnScrollingCandidatePanel];
+        
+        [[NSApplication sharedApplication] run];
+    }
+    
+    return 0;
 }

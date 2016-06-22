ce84d449e6bfae4375819daec86826b379124c8e
Peter Kamb <peterkamb@gmail.com>
2016-06-21 21:30:17 -0700

    Add `IMKBoilerplateInputController`

---
+++ InputMethodKitBoilerplate/InputMethodKitBoilerplate/IMKBoilerplateInputController.h
@@ -0,0 +1,13 @@
+//
+//  IMKBoilerplateInputController.h
+//  InputMethodKitBoilerplate
+//
+//  Created by Peter Kamb on 6/21/16.
+//  Copyright © 2016 Peter Kamb. All rights reserved.
+//
+
+#import <InputMethodKit/InputMethodKit.h>
+
+@interface IMKBoilerplateInputController : IMKInputController
+
+@end
---
+++ InputMethodKitBoilerplate/InputMethodKitBoilerplate/IMKBoilerplateInputController.m
@@ -0,0 +1,13 @@
+//
+//  IMKBoilerplateInputController.m
+//  InputMethodKitBoilerplate
+//
+//  Created by Peter Kamb on 6/21/16.
+//  Copyright © 2016 Peter Kamb. All rights reserved.
+//
+
+#import "IMKBoilerplateInputController.h"
+
+@implementation IMKBoilerplateInputController
+
+@end
